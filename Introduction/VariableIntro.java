3b0f9e5b8612ae17c03e0ee3a8d8bc02a86af273
15273 <ryan.mauro@vtsd.com>
2025-05-27 13:55:12 -0400

    Started Java

---
+++ Introduction/VariableIntro.java
@@ -0,0 +1,19 @@
+    package Introduction;
+
+    public class VariableIntro {
+        public static void main(String[] args) {
+
+            double salary = 10000.56;
+            int bonus = 250;
+
+            bonus = (int) salary;
+            System.out.println(salary);
+            System.out.println(bonus);
+
+
+
+
+
+
+        }
+}
---
+++ Introduction/src/Hello_World.java
@@ -0,0 +1,14 @@
+//Create class Hello_World
+public class Hello_World {
+
+
+
+    //Create main method
+    public static void main(String[] args) {
+        //Output to the main window hello world
+        System.out.println("Hello World");
+
+
+
+    }
+}
