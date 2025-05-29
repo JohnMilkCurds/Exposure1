cd957f696ac9e34ed064e164cc37222687e93877
15273 <ryan.mauro@vtsd.com>
2025-05-29 12:28:19 -0400

    Started Lab03

--- Introduction/src/VariableIntro.java
+++ Introduction/src/VariableIntro.java
@@ -1,12 +1,12 @@
-    package Introduction;
-
-    public class VariableIntro {
+public class VariableIntro {
         public static void main(String[] args) {
 
             double salary = 10000.56;
-            int bonus = 250;
+            int bonus;
+
 
             bonus = (int) salary;
+
             System.out.println(salary);
             System.out.println(bonus);
 
---
+++ Lab03/Lab03.java
@@ -0,0 +1,20 @@
+package Lab03;
+
+public class Lab03 {
+    public static void main(String[] args) {
+
+        int start_time = 10000123;
+        int hours = start_time / 3600000;
+        int remaining_seconds = start_time % 3600000;
+        int minutes = remaining_seconds / 60000;
+        int seconds = remaining_seconds % 60;
+        int milliseconds = remaining_seconds % 1000;
+
+        System.out.println("Starting time: \t" + start_time);
+        System.out.println("Hours: \t\t\t\t\t\t" + hours);
+        System.out.println("Minutes: \t\t\t\t\t" + minutes);
+        System.out.println("Seconds: \t\t\t\t\t" + seconds);
+        System.out.println("Milliseconds: \t\t\t\t" + milliseconds);
+
+    }
+}
